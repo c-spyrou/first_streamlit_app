76247696db042333133a87618f9d526f4bd44bf2
c-spyrou <141027984+c-spyrou@users.noreply.github.com>
2023-07-31 12:10:17 +0100

    Update streamlit_app.py
    Changed menu and added emoijs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Eggs Benedict')
-streamlit.text('Salmon and Avocado Bagel')
-streamlit.text('New York style Pancakes')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Egg')
+streamlit.text('🥑🍞 Avocado Toast')
